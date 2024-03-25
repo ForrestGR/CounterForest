e7fea2834a3537170c1d8b045cc90a261b40a525
ForrestGR <f.brede@hotmail.de>
2024-03-25 01:49:53 +0100

    update

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,7 +6,11 @@ public class Bullet : MonoBehaviour
 {
 
     public float Speed = 20f;
+    public int damage = 40;
+
     public Rigidbody2D rb;
+
+    public GameObject impactEffect;
 
 
     // Start is called before the first frame update
@@ -15,5 +19,26 @@ public class Bullet : MonoBehaviour
         rb.velocity = transform.right * Speed;
     }
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
 
 }
