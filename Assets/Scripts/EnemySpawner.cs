7047f012d79bac48e7cf5e0fc1ef0766ab8f8061
ForrestGR <f.brede@hotmail.de>
2024-03-26 14:36:21 +0100

    update

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,10 +7,7 @@ public class EnemySpawner : MonoBehaviour
     public GameObject[] enemyPrefabs; // Ein Array von Gegner-Prefabs zum Spawnen
     public float spawnDelay = 2f; // Verzögerung zwischen den Spawn-Vorgängen
     public float spawnRadius = 5f; // Radius für zufällige Spawn-Positionen um den Spawner
-
-
-
-
+    public Transform playerTransform; // Referenz auf den Spieler, der verfolgt werden soll
 
     private void Start()
     {
@@ -22,10 +19,13 @@ public class EnemySpawner : MonoBehaviour
         while (true) // Eine Endlosschleife, die Gegner spawnen lässt
         {
             yield return new WaitForSeconds(spawnDelay); // Wartet für die festgelegte Verzögerungszeit
-            
-            Vector2 spawnPosition = Random.insideUnitCircle * spawnRadius + (Vector2)transform.position;   // Erzeugt eine zufällige Position im definierten Radius um den Spawner
+
+            Vector2 spawnPosition = Random.insideUnitCircle * spawnRadius + (Vector2)transform.position; // Erzeugt eine zufällige Position im definierten Radius um den Spawner
             GameObject enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];  // Wählt ein zufälliges Gegner-Prefab aus
-            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);  // Spawnt den Gegner an der berechneten Position
+            GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);  // Spawnt den Gegner an der berechneten Position
+
+            // Setzt die Spielerreferenz im Enemy-Skript
+            enemy.GetComponent<Enemy>().player = playerTransform;
         }
     }
 }
